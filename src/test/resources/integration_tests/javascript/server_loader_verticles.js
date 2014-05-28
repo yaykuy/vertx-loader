81522c3c03ae95315da4beaf34c0dfd73b65c6f9
Andres Junge <ajunge@yaykuy.cl>
2014-05-28 03:52:42 -0400

    Some 0 test

--- src/test/resources/integration_tests/javascript/server_loader_verticles.js
+++ src/test/resources/integration_tests/javascript/server_loader_verticles.js
@@ -51,7 +51,7 @@ function testVerticles(){
 	{
 			"appConfig": {
 				"verticles" : {
-					"some": {}
+					"some0": {}
 				}
 			}
 	};
---
+++ src/test/resources/verticles/some0_verticle.js
@@ -0,0 +1,5 @@
+var vertx     = require('vertx');
+var container = require('vertx/container');
+var console = require('vertx/console');
+
+console.log("some 0.Ready");
